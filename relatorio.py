9c43113ff1c5e93ea76e56513bdebb34a7b724fe
Luan <luanfraga02@gmail.com>
2026-03-30 21:41:13 -0300

    Adicionado relatorio de notas

---
+++ relatorio.py
@@ -0,0 +1,18 @@
+# dados fictícios
+nome = "João"
+idade = 16
+nota = 8.5
+
+# criando o relatório
+with open("relatorio.txt", "w", encoding="utf-8") as arquivo:
+    arquivo.write("=== RELATÓRIO ===\n")
+    arquivo.write(f"Nome: {nome}\n")
+    arquivo.write(f"Idade: {idade}\n")
+    arquivo.write(f"Nota final: {nota}\n")
+    
+    if nota >= 7:
+        arquivo.write("Status: Aprovado\n")
+    else:
+        arquivo.write("Status: Reprovado\n")
+
+print("Relatório gerado com sucesso!")
